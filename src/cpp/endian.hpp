2805caaadf1e7aa60fda65274d928dfa8a789ae5
Thomas B. Preußer <thomas.preusser@utexas.edu>
2015-09-24 11:46:04 +0200

    Updated license headers in source files.

--- src/cpp/endian.hpp
+++ src/cpp/endian.hpp
@@ -5,22 +5,18 @@
  * Copyright (C) 2008-2015
  *      Thomas B. Preusser <thomas.preusser@utexas.edu>
  *****************************************************************************
- * This design is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Modifications to this work must be clearly identified and must leave
- * the original copyright statement and contact information intact. This
- * license notice may not be removed.
- *
- * This design is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this design.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ****************************************************************************/
 #ifndef ENDIAN_HPP
 #define ENDIAN_HPP
